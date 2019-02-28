bb02631f9bc6785b071808d523aaaf45acb035a8
hendisantika <hendisantika@yahoo.co.id>
2019-02-28 07:10:23 +0700

    #7 - Modify Main Test Case

--- src/test/java/com/hendisantika/springbootsftpuploaddemo/SpringbootSftpUploadDemoApplicationTests.java
+++ src/test/java/com/hendisantika/springbootsftpuploaddemo/SpringbootSftpUploadDemoApplicationTests.java
@@ -1,16 +1,73 @@
 package com.hendisantika.springbootsftpuploaddemo;
 
-import org.junit.Test;
+import com.hendisantika.springbootsftpuploaddemo.config.SftpConfig;
+import org.junit.*;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(properties = {"sftp.port = 10022"})
 public class SpringbootSftpUploadDemoApplicationTests {
 
+    private static EmbeddedSftpServer server;
+    private static Path sftpFolder;
+    @Autowired
+    private SftpConfig.UploadGateway gateway;
+
+    @BeforeClass
+    public static void startServer() throws Exception {
+        server = new EmbeddedSftpServer();
+        server.setPort(10022);
+        sftpFolder = Files.createTempDirectory("SFTP_UPLOAD_TEST");
+        server.afterPropertiesSet();
+        server.setHomeFolder(sftpFolder);
+        // Starting SFTP
+        if (!server.isRunning()) {
+            server.start();
+        }
+    }
+
+    @AfterClass
+    public static void stopServer() {
+        if (server.isRunning()) {
+            server.stop();
+        }
+    }
+
+    @Before
+    @After
+    public void cleanSftpFolder() throws IOException {
+        Files.walk(sftpFolder).filter(Files::isRegularFile).map(Path::toFile).forEach(File::delete);
+    }
+
     @Test
-    public void contextLoads() {
+    public void testUpload() throws IOException {
+        // Prepare phase
+        Path tempFile = Files.createTempFile("UPLOAD_TEST", ".csv");
+
+        // Prerequisites
+        assertEquals(0, Files.list(sftpFolder).count());
+
+        // test phase
+        gateway.upload(tempFile.toFile());
+
+        // Validation phase
+        List<Path> paths = Files.list(sftpFolder).collect(Collectors.toList());
+        assertEquals(1, paths.size());
+        assertEquals(tempFile.getFileName(), paths.get(0).getFileName());
     }
 
 }
