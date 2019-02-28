c029e8f50013326add107c36eec5d3c7e167977d
hendisantika <hendisantika@yahoo.co.id>
2019-02-28 07:07:23 +0700

    #5 - Add Main Class

--- src/main/java/com/hendisantika/springbootsftpuploaddemo/SpringbootSftpUploadDemoApplication.java
+++ src/main/java/com/hendisantika/springbootsftpuploaddemo/SpringbootSftpUploadDemoApplication.java
@@ -2,8 +2,12 @@ package com.hendisantika.springbootsftpuploaddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.config.EnableIntegration;
 
 @SpringBootApplication
+@IntegrationComponentScan
+@EnableIntegration
 public class SpringbootSftpUploadDemoApplication {
 
     public static void main(String[] args) {
