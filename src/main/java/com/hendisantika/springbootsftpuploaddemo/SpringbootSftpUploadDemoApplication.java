a598becc43fb3a44a3e6e06f6b7aefda195d5180
hendisantika <hendisantika@yahoo.co.id>
2019-02-28 07:04:18 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootsftpuploaddemo/SpringbootSftpUploadDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootsftpuploaddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSftpUploadDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSftpUploadDemoApplication.class, args);
+    }
+
+}
